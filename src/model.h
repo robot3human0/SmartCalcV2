04f79cce57ccef0c78b1f45e35bff6cb491da581
Myesha Skaar <myeshask@si-a5.nsk.21-school.ru>
2023-09-23 19:06:27 +0700

    back_done

---
+++ src/model.h
@@ -0,0 +1,103 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <iostream>
+#include <variant>
+#include <string>
+#include <functional>
+#include <regex>
+#include <stack>
+#include <map>
+#include <cmath>
+#include <queue>
+#include <list>
+#include <iomanip>
+
+namespace s21 {
+    template <class... Ts>
+    struct overloaded : Ts... {
+        using Ts::operator()...;
+    };
+
+    template <class... Ts>
+    overloaded(Ts...) -> overloaded<Ts...>;
+
+    enum Precedence {
+        Zero, // числа, скобки, все что не функция
+        lBracket, // левая скобка
+        Low, // плюс,минус
+        Medium, // умножить поделить остаток от деления
+        High, // возведение в степень
+        Ultra, // функция
+        rBracket, // правая скобка
+    };
+
+    enum Associativity {
+        Nope,
+        Left, // минус деление остаток от деления
+        Right, // степень
+    };
+
+    enum Type {
+        Num, // число
+        oBinary, // бинарный оператор
+        oUnary, // унарный оператор
+        Bracket,
+    };
+
+    using fUnary = std::function<double(double)>;
+    using fBinary = std::function<double(double, double)>;
+    using FuVariant = std::variant<double, fUnary, fBinary, std::nullptr_t>;
+
+    class Token {
+    public:
+        Token() = default;
+        Token(const std::string& name, Precedence precedence, 
+              Associativity associativity, Type type, FuVariant function);
+        ~Token() = default;
+
+        std::string GetName();
+        Precedence GetPrecedence();
+        Associativity GetAssociativity();
+        Type GetType();
+        FuVariant GetFuVariant();
+
+    private:
+        std::string name_;
+        Precedence precedence_;
+        Associativity associativity_;
+        Type type_;
+        FuVariant function_;
+    };
+
+    class Calculator {
+    public:
+        Calculator();
+        Calculator(std::string& expression, double x);
+        ~Calculator() = default;
+
+        double GetAnswer();
+        std::string GetExpression();
+        void SetX(double newX);
+        void Calc();
+
+    private:
+        void Validate();
+        void IsBracketsPaired();
+        void CreateTokenMap();
+        void MakeCalculation();
+        void ToRPN(const std::string& lexeme);
+        void FindUnaryOperatorsAndAddAZero();
+
+        double answer_;
+        double x_;
+        std::string expression_;
+        std::stack<double> numbers_;
+        std::stack<s21::Token> operators_;
+        std::queue<s21::Token> rpnLine_;
+        std::map<std::string, s21::Token> tokenMap_;
+    };
+
+} // namespace s21
+
+#endif  // MODEL_H
